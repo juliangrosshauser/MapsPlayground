f88e2375e0c1b823d728cf0445729ba986263215
Julian Grosshauser <julian@juliangrosshauser.com>
2015-06-17 23:45:06 +0200

    Add "Display location in map view" section

--- Maps.playground/Contents.swift
+++ Maps.playground/Contents.swift
@@ -1,4 +1,5 @@
 import CoreLocation
+import MapKit
 
 /*:
 # Get current location
@@ -34,3 +35,13 @@ class LocationManager: NSObject, CLLocationManagerDelegate {
 }
 
 let locationManager = LocationManager()
+
+/*:
+# Display location in map view
+Based on Apple's ["Displaying Maps"](https://developer.apple.com/library/ios/documentation/UserExperience/Conceptual/LocationAwarenessPG/MapKit/MapKit.html#//apple_ref/doc/uid/TP40009497-CH3-SW1)
+*/
+
+let mapView = MKMapView(frame: CGRect(x: 0, y: 0, width: 500, height: 500))
+
+// show New York in map view
+mapView.region = MKCoordinateRegionMakeWithDistance(CLLocationCoordinate2D(latitude: 40.7141667, longitude: -74.0063889), 5000, 5000)
